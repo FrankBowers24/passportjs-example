facd0382a186d3da0ef1019444246cddec37a2cf
FrankBowers24 <FrankBowers24@gmail.com>
2015-05-05 10:32:37 -0700

    Rename files and remove Spanish comments for merge with SocialCapital

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-// Archivo principal del Backend, configuración del servidor
-// y otras opciones
 
-var express = require('express'); // Express: Framework HTTP para Node.js
-var routes = require('./routes'); // Dónde tenemos la configuración de las rutas
+
+var express = require('express'); 
+var routes = require('./routes'); 
 var path = require('path');
 
-var mongoose = require('mongoose'); // Mongoose: Libreria para conectar con MongoDB
-var passport = require('passport'); // Passport: Middleware de Node que facilita la autenticación de usuarios
+var mongoose = require('mongoose'); 
+var passport = require('passport'); 
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -14,32 +13,20 @@ var session = require('express-session');
 var methodOverride = require('method-override');
 var errorHandler = require('errorhandler');
 
-// Importamos el modelo usuario y la configuración de passport
 require('./models/user');
-require('./passport')(passport);
+require('./authPassport')(passport);
 
-// Conexión a la base de datos de MongoDB que tenemos en local
 mongoose.connect('mongodb://localhost:27017/passport-example', function(err, res) {
   if(err) throw err;
   console.log('Conectado con éxito a la BD');
 });
 
-// Iniciamos la aplicación Express
 var app = express();
 
-// Configuración (Puerto de escucha, sistema de plantillas, directorio de vistas,...)
 app.set('port', process.env.PORT || 5000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-// app.use(express.favicon());
-// app.use(express.logger('dev'));
 
-// Middlewares de Express que nos permiten enrutar y poder
-// realizar peticiones HTTP (GET, POST, PUT, DELETE)
-// app.use(express.cookieParser());
-// app.use(express.urlencoded());
-// app.use(express.json());
-// app.use(express.methodOverride());
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -47,55 +34,43 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser('add a sign here'));
 app.use(methodOverride());
 
-// Ruta de los archivos estáticos (HTML estáticos, JS, CSS,...)
 app.use(express.static(path.join(__dirname, 'public')));
-// Indicamos que use sesiones, para almacenar el objeto usuario
-// y que lo recuerde aunque abandonemos la página
-app.use(session({ secret: 'lollllo' }));
 
-// Configuración de Passport. Lo inicializamos
-// y le indicamos que Passport maneje la Sesión
+app.use(session({ secret: 'xyz-qwrty' }));
+
+// passport initialization
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.Router());
 
-// Si estoy en local, le indicamos que maneje los errores
-// y nos muestre un log más detallado
+
 if ('development' == app.get('env')) {
   app.use(errorHandler());
 }
 
-/* Rutas de la aplicación */
-// Cuando estemos en http://localhost:puerto/ (la raiz) se ejecuta el metodo index
-// del modulo 'routes'
+
 app.get('/', routes.index);
 
-/* Rutas de Passport */
-// Ruta para desloguearse
+/* Passport Routes */
 app.get('/logout', function(req, res) {
   req.logout();
   res.redirect('/');
 });
-// Ruta para autenticarse con Twitter (enlace de login)
 app.get('/auth/twitter', passport.authenticate('twitter'));
-// Ruta para autenticarse con Facebook (enlace de login)
 app.get('/auth/facebook', passport.authenticate('facebook'));
-// Ruta de callback, a la que redirigirá tras autenticarse con Twitter.
-// En caso de fallo redirige a otra vista '/login'
 app.get('/auth/twitter/callback', passport.authenticate('twitter',
   { successRedirect: '/', failureRedirect: '/login' }
 ));
-// Ruta de callback, a la que redirigirá tras autenticarse con Facebook.
-// En caso de fallo redirige a otra vista '/login'
 app.get('/auth/facebook/callback', passport.authenticate('facebook',
   { successRedirect: '/', failureRedirect: '/login' }
 ));
+
+//============  end passport routes
 app.get('/api/test', function(req, res) {
 	console.log('api/test session: ', req.session, 'request user: ', req.user);
 	res.send('api/test get received');
 })
 
-// Inicio del servidor
 app.listen(app.get('port'), function(){
   console.log('Aplicación Express escuchando en el puerto ' + app.get('port'));
 });
